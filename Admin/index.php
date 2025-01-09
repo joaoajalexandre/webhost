26e7da46c964d08c8f5d82c1349af04c305d6e64
Marcelino Lufendo <lufendomarcelino07@gmail.com>
2025-01-09 01:44:56 +0100

    usuarios e serviços

--- Admin/index.php
+++ Admin/index.php
@@ -49,82 +49,9 @@ include("conexao.php");
   <body>
     <div class="wrapper">
       <!-- Sidebar -->
-      <div class="sidebar" data-background-color="dark">
-        <div class="sidebar-logo">
-          <!-- Logo Header -->
-          <div class="logo-header" data-background-color="dark">
-            <a href="index.php" class="logo">
-              <img
-                src="../img/logo.png"
-                alt="navbar brand"
-                class="navbar-brand"
-                height="20"
-              />
-            </a>
-            <div class="nav-toggle">
-              <button class="btn btn-toggle toggle-sidebar">
-                <i class="gg-menu-right"></i>
-              </button>
-              <button class="btn btn-toggle sidenav-toggler">
-                <i class="gg-menu-left"></i>
-              </button>
-            </div>
-            <button class="topbar-toggler more">
-              <i class="gg-more-vertical-alt"></i>
-            </button>
-          </div>
-          <!-- End Logo Header -->
-        </div>
-        <div class="sidebar-wrapper scrollbar scrollbar-inner">
-          <div class="sidebar-content">
-            <ul class="nav nav-secondary">
-              <li class="nav-item active">
-                <a
-                  data-bs-toggle="collapse"
-                  href="#dashboard"
-                  class="collapsed"
-                  aria-expanded="false"
-                >
-                  <i class="fas fa-home"></i>
-                  <p>Home</p>
-                  <span class="caret"></span>
-                </a>
-                <div class="collapse" id="dashboard">
-                  <ul class="nav nav-collapse">
-                    <li>
-                      <a href="index.php">
-                        <span class="sub-item">Dashboard</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li class="nav-section">
-                <span class="sidebar-mini-icon">
-                  <i class="fa fa-ellipsis-h"></i>
-                </span>
-                <h4 class="text-section">paginas</h4>
-              </li>
-              
-              <li class="nav-item">
-                <a href="./clientes.php">
-                  <i class="fas fa-file"></i>
-                  <p>clientes</p>
-                  <span class="badge badge-secondary"></span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./dominios_comprados.php">
-                  <i class="fas fa-file"></i>
-                  <p>vendas</p>
-                  <span class="badge badge-secondary"></span>
-                </a>
-              </li>
-              
-            </ul>
-          </div>
-        </div>
-      </div>
+      <?php
+        include("sidebar.php");
+        ?>
       <!-- End Sidebar -->
 
       <div class="main-panel">
